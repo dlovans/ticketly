1b2948b1da7e4c5ecce7f438cf034a842174fc0d
Lvhacker1 <lavasha2@hotmail.com>
2025-12-30 18:17:42 +0100

    small fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Navbar from "$lib/components/layout/Navbar.svelte";
+	import Footer from "$lib/components/layout/Footer.svelte";
 </script>
 
 <Navbar />
 <h1>Welcome to Ticketly</h1>
+<Footer />
